bed560fa49f6f7daba674d4f156d4741c1677342
kjod <kieranjamesodriscoll@gmail.com>
2018-03-02 18:49:04 +0100

    Sample map created

---
+++ client/js/map.js
@@ -0,0 +1,147 @@
+function createMap() {
+  console.log("here");
+  var mapProp= {
+      center:new google.maps.LatLng(52.3702, 4.8952),
+      zoom:11,
+      disableDefaultUI: true,
+      styles:[
+        {
+          "featureType": "landscape.man_made",
+          "elementType": "all",
+          "stylers": [
+                {
+                  "color": "#444444"
+                },
+                {
+                    "saturation": "-100"
+                }
+            ]
+        },
+        {
+          "featureType": "administrative.neighborhood",
+          "elementType": "all",
+          "stylers": [
+                {
+                    "saturation": "-100"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "saturation": "-100"
+                },
+                {
+                    "lightness": "65"
+                },
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "saturation": "-100"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "lightness": "30"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "lightness": "40"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "saturation": -100
+                },
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "hue": "#ffff00"
+                },
+                {
+                    "lightness": -25
+                },
+                {
+                    "saturation": -97
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+      ],
+  };
+  var map=new google.maps.Map(document.getElementById("map"),mapProp);
+  console.log("should be a map");
+}
