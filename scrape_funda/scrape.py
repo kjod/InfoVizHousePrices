47009350d4963ad12b1b162fa146d578b2ad99fb
tushar1408 <tushar.1408@gmail.com>
2018-03-05 18:57:34 +0100

    rooms and area added

--- scrape_funda/scrape.py
+++ scrape_funda/scrape.py
@@ -7,7 +7,7 @@ import pandas as pd
 import time
 from random import randint
 
-browser = webdriver.Chrome('/home/tushar/Downloads/Funda-Scraper-master/chromedriver')
+browser = webdriver.Chrome('/home/tushar/Downloads/Funda-Scraper-master/chromedriver')#put your driver location
 
 browser.implicitly_wait(15)
 wait = WebDriverWait(browser, 30)
@@ -36,16 +36,22 @@ for url in urls:
 
             house_price = item.find_element_by_css_selector("span.search-result-price").text.lstrip('€ ').rstrip(
                 ' k.k,').replace('.', '')
+            area, rooms = item.find_element_by_css_selector("ul.search-result-kenmerken").text.replace('\n','').replace('m²', '').split(" ", 1)
+            if '/' in rooms:
+                rooms=rooms[7:]
+            rooms = rooms.replace('kamer', '').replace('s', '')
 
             data.append({
                 "street_name":street_name,
                 "zipcode":zipcode,
                 "house_price":house_price,
+                "area":area,
+                "rooms":rooms,
             })
         except ValueError:
             pass
 
-    time.sleep(randint(10,15))
+    time.sleep(randint(5,10))
 
 browser.close()
 df = pd.DataFrame(data)
