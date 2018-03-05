38edb847d71668e82518369464525ae4909d2ba3
tushar1408 <tushar.1408@gmail.com>
2018-03-05 18:05:44 +0100

    get elements by css

--- scrape_funda/scrape.py
+++ scrape_funda/scrape.py
@@ -6,3 +6,23 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 from random import randint
+
+browser = webdriver.Chrome('/home/tushar/Downloads/Funda-Scraper-master/chromedriver')
+
+browser.implicitly_wait(15)
+wait = WebDriverWait(browser, 30)
+urls = [
+        'http://www.funda.nl/koop/amsterdam/p',
+    ]
+
+data = []
+
+pages = range(1,139)
+
+for url in urls:
+	for page in pages:
+		browser.get(url+str(page))
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.search-result-content")))
+
+        for item in browser.find_elements_by_css_selector("div.search-result-content"):
+        	print(item)
