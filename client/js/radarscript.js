3b417ed23875dc29154df66dc01d2fb8f8ad1614
danielstaal <daniel.staal@hotmail.com>
2018-03-19 17:57:06 +0100

    radarplot

--- client/js/radarscript.js
+++ client/js/radarscript.js
@@ -19,49 +19,75 @@ var mycfg = {
 //Call function to draw the Radar chart
 //Will expect that data is in %'s
 function initGraph(area_code, area_name){
-  //Data crimeindex, popDensity, energy, houseprices, nationalities
-  var selected_region = [0.0,0.0,0.0,0.0]
-  var average_all_regions = [0.5,0.5,0.5,0.5]
+  // variables:
+  var variableNames = ["Crime index", "Pop. density", "Energy label", "WOZ value", "Green areas"]
+  //  
+
+  // initialize list for the selected region variables
+  var selected_region = Array.apply(null, Array(variableNames.length)).map(Number.prototype.valueOf,0);
+
+  // data
+  var data = []
+  for(var i=0; i<variableNames.length;i++){
+    data.push([])
+  }
+
   var area_clicked = null
 
   d3.json("names_coordinates_data/districts.json", function(shapes) {
       var counter = 0
       shapes.Areas.forEach(function(d){
         counter += 1
+
+        // variables
+        var crimeIndex = Number(d.Crime_index_2016)
+        var popDensity = Number(d.Population_density_2016)
+        var energy = Number(d.energy_label_2016)
+        var woz = Number(d.WOZ_value_2016)
+        var green = Number(d.surface_green_2016)
+
+        var variableValues = [crimeIndex, popDensity, energy, woz, green]
+
+        // if this region is selected collect data
         if (d.area_code === area_code){
-          selected_region[0] = d.Crime_index_2016;
-          selected_region[1] = d.Population_density_2016;
-          selected_region[2] = d.energy_label_2016;
-          selected_region[3] = d.WOZ_value_2016;
+          for (var i=0; i<variableValues.length; i++){
+            selected_region[i] = variableValues[i]
+          }
         }
-        // average_all_regions[0] += d.Crime_index_2016;
-        // average_all_regions[1] += d.Population_density_2016;
-        // average_all_regions[2] += d.energy_label_2016;
-        // average_all_regions[3] += d.WOZ_value_2016;
+        // collect data for all regions
+        for(var i=0; i<variableValues.length; i++){
+          if (isNumeric(variableValues[i]) && variableValues[i] != 0){
+            data[i].push(variableValues[i])
+          }
+        }
       })
-      // average_all_regions[0] /= counter
-      // average_all_regions[1] /= counter
-      // average_all_regions[2] /= counter
-      // average_all_regions[3] /= counter
-
-      console.log(selected_region)
-      console.log(average_all_regions)
-
+      var average_all_regions = []
+      for (var i=0; i<data.length; i++){
+        var sum = 0
+        for (var j=0; j<data[i].length; j++){
+          if (!isNaN(data[i][j])){
+            sum += data[i][j]
+          }
+        }
+        average_all_regions.push(sum/data[i].length)
+      }
 
     //Legend titles
     var LegendOptions = [area_name, 'Average of all regions'];
 
     var d = [
         [
-        {axis:"Crime index",value:selected_region[0]},
-        {axis:"Population density",value:selected_region[1]},
-        {axis:"Energy label",value:selected_region[2]},
-        {axis:"WOV Value",value:selected_region[3]},
+        {axis:variableNames[0],value:applyNormalization(data[0],selected_region[0])},
+        {axis:variableNames[1],value:applyNormalization(data[1],selected_region[1])},
+        {axis:variableNames[2],value:applyNormalization(data[2],selected_region[2])},
+        {axis:variableNames[3],value:applyNormalization(data[3],selected_region[3])},
+        {axis:variableNames[4],value:applyNormalization(data[4],selected_region[4])},
         ],[
-        {axis:"Crime index",value:average_all_regions[0]},
-        {axis:"Population density",value:average_all_regions[1]},
-        {axis:"Energy label",value:average_all_regions[2]},
-        {axis:"WOZ Value",value:average_all_regions[3]},
+        {axis:variableNames[0],value:applyNormalization(data[0], average_all_regions[0])},
+        {axis:variableNames[1],value:applyNormalization(data[1],average_all_regions[1])},
+        {axis:variableNames[2],value:applyNormalization(data[2],average_all_regions[2])},
+        {axis:variableNames[3],value:applyNormalization(data[3],average_all_regions[3])},
+        {axis:variableNames[4],value:applyNormalization(data[4],average_all_regions[4])},
         ]
       ];
 
@@ -72,7 +98,7 @@ function initGraph(area_code, area_name){
     .attr("width", w+300)
     .attr("height", h)
 
-  // //Create the title for the legend
+  //Create the title for the legend
   // var text = svg.append("text")
   //   .attr("class", "title")
   //   .attr('transform', 'translate(90,10)') 
@@ -113,6 +139,28 @@ function initGraph(area_code, area_name){
       ; 
 
   })
+}
+
+function applyNormalization(data, point){
+  var maxValue = Math.max(...data)
+  var minValue = Math.min(...data)
+
+  // for(var i=0;i<data.length;i++){
+  //   console.log("value",data[i])
+  // }
+
+  console.log("data",data)
+  console.log("point", point)
+  console.log("max", maxValue)
+  console.log("min", minValue)
+
+  var normalized_point = (point - minValue) / (maxValue - minValue)
+  console.log("norm", normalized_point)
+  return normalized_point
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 ////////////////////////////////////////////
 /////////// Initiate legend ////////////////
