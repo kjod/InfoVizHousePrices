596330ac40acd86d3eb20aef16294565c0f2f5f1
kjod <kieranjamesodriscoll@gmail.com>
2018-03-22 13:05:21 +0100

    Tried changing radar plot

--- client/js/radarscript.js
+++ client/js/radarscript.js
@@ -94,53 +94,54 @@ function initGraph(area_code, area_name){
       }
     }
 
-    RadarChart.draw("#overviewChart", d, mycfg);
-    var svg = d3.select('#overviewChart')
-    .selectAll('svg')
-    .append('svg')
-    .attr("width", w+300)
-    .attr("height", h)
+    if(d.length > 0){
+      RadarChart.draw("#overviewChart", d, mycfg);
+      var svg = d3.select('#overviewChart')
+      .selectAll('svg')
+      .append('svg')
+      .attr("width", w+300)
+      .attr("height", h)
 
-  //Create the title for the legend
-  // var text = svg.append("text")
-  //   .attr("class", "title")
-  //   .attr('transform', 'translate(90,10)') 
-  //   .attr("x", w - 70)
-  //   .attr("y", 10)
-  //   .attr("font-size", "20px")
-  //   .attr("fill", "#404040")
-  //   .text("Legend");
-      
-  //Initiate Legend 
-  var legend = svg.append("g")
-    .attr("class", "legend")
-    .attr("height", 100)
-    .attr("width", 200)
-    .attr('transform', 'translate(90,20)') 
-    ;
-    //Create colour squares
-    legend.selectAll('rect')
-      .data(LegendOptions)
-      .enter()
-      .append("rect")
-      .attr("x", w - 65)
-      .attr("y", function(d, i){ return i * 25 + 9;})
-      .attr("width", 10)
-      .attr("height", 10)
-      .style("fill", function(d, i){ return colorscale(i);})
+    //Create the title for the legend
+    // var text = svg.append("text")
+    //   .attr("class", "title")
+    //   .attr('transform', 'translate(90,10)') 
+    //   .attr("x", w - 70)
+    //   .attr("y", 10)
+    //   .attr("font-size", "20px")
+    //   .attr("fill", "#404040")
+    //   .text("Legend");
+        
+    //Initiate Legend 
+    var legend = svg.append("g")
+      .attr("class", "legend")
+      .attr("height", 100)
+      .attr("width", 200)
+      .attr('transform', 'translate(90,20)') 
       ;
-    //Create text next to squares
-    legend.selectAll('text')
-      .data(LegendOptions)
-      .enter()
-      .append("text")
-      .attr("x", w - 52)
-      .attr("y", function(d, i){ return i * 25 + 20;})
-      .attr("font-size", "22px")
-      .attr("fill", "#737373")
-      .text(function(d) { return d; })
-      ; 
-
+      //Create colour squares
+      legend.selectAll('rect')
+        .data(LegendOptions)
+        .enter()
+        .append("rect")
+        .attr("x", w - 65)
+        .attr("y", function(d, i){ return i * 25 + 9;})
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", function(d, i){ return colorscale(i);})
+        ;
+      //Create text next to squares
+      legend.selectAll('text')
+        .data(LegendOptions)
+        .enter()
+        .append("text")
+        .attr("x", w - 52)
+        .attr("y", function(d, i){ return i * 25 + 20;})
+        .attr("font-size", "22px")
+        .attr("fill", "#737373")
+        .text(function(d) { return d; })
+        ; 
+    }
   })
 }
 
